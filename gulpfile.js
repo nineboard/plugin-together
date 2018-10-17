30c47590a25649f849b79f617d5e19e7a9ff9522
bnu <bnufactory@gmail.com>
2018-10-17 17:16:56 +0900

    테마 플러그인 기초 구성 추가

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp')
+var $ = require('gulp-load-plugins')()
+var autoprefixer = require('gulp-autoprefixer')
+
+var config = {
+  isProduction: !!$.util.env.production,
+  useSourceMaps: !$.util.env.production
+}
+
+gulp.task('default', ['sass'])
+
+gulp.task('sass', function () {
+  return gulp.src(['./assets/scss/style.scss', '!_*.scss'])
+    .pipe($.if(config.useSourceMaps, $.sourcemaps.init()))
+    .pipe($.plumber())
+    .pipe($.sass({outputStyle: 'compressed'}).on('error', $.sass.logError))
+    .pipe(autoprefixer({
+      browsers: [
+        'last 2 versions'
+      ],
+      cascade: false
+    }))
+    .pipe($.if(config.useSourceMaps, $.sourcemaps.write('.')))
+    .pipe(gulp.dest('./assets/css'))
+})
+
+gulp.task('watch', function () {
+  gulp.watch('./assets/scss/*.scss', ['sass'])
+})
