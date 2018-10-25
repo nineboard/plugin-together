7cb19db775ff1d238b038fa7fb4931e45e4c6d92
bnu <bnufactory@gmail.com>
2018-10-25 11:08:40 +0900

    메뉴 및 테마 옵션 적용

--- Components/Theme/views/_footer.blade.php
+++ Components/Theme/views/_footer.blade.php
@@ -1,85 +1,20 @@
 <footer class="footer-tfc">
     <div class="inner-footer">
-        <small>Copyright ⓒ 2012 SM Entertainment Co., Ltd. <span class="align">All rights reserved</span></small>
+        @if($config->get('useCopyright') !== 'N')
+            @if(trim($config->get('copyrightContent')))
+                <small>{!! $config->get('copyrightContent') !!}</small>
+            @else
+                Powered by <a href="https://www.xpressengine.io/" target="_blank">XpressEngine</a>.
+            @endif
+        @endif
         <div class="area_language reset-button">
             <button type="button">
                 <span class="icon_lang_korea">
-                    <span class="path1"></span>
-                    <span class="path2"></span>
-                    <span class="path3"></span>
-                    <span class="path4"></span>
-                    <span class="path5"></span>
-                    <span class="path6"></span>
-                    <span class="path7"></span>
-                    <span class="path8"></span>
-                    <span class="path9"></span>
-                    <span class="path10"></span>
-                    <span class="path11"></span>
-                    <span class="path12"></span>
-                    <span class="path13"></span>
-                    <span class="path14"></span>
-                    <span class="path15"></span>
-                    <span class="path16"></span>
-                    <span class="path17"></span>
-                    <span class="path18"></span>
-                    <span class="path19"></span>
-                    <span class="path20"></span>
-                    <span class="path21"></span>
-                </span>
-                <span class="blind">대한민국</span>
+                    <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span><span class="path20"></span><span class="path21"></span></span><span class="blind">대한민국</span>
             </button>
             <button type="button">
                 <span class="icon_lang_unstates">
-                    <span class="path1"></span>
-                    <span class="path2"></span>
-                    <span class="path3"></span>
-                    <span class="path4"></span>
-                    <span class="path5"></span>
-                    <span class="path6"></span>
-                    <span class="path7"></span>
-                    <span class="path8"></span>
-                    <span class="path9"></span>
-                    <span class="path10"></span>
-                    <span class="path11"></span>
-                    <span class="path12"></span>
-                    <span class="path13"></span>
-                    <span class="path14"></span>
-                    <span class="path15"></span>
-                    <span class="path16"></span>
-                    <span class="path17"></span>
-                    <span class="path18"></span>
-                    <span class="path19"></span>
-                    <span class="path20"></span>
-                    <span class="path21"></span>
-                    <span class="path22"></span>
-                    <span class="path23"></span>
-                    <span class="path24"></span>
-                    <span class="path25"></span>
-                    <span class="path26"></span>
-                    <span class="path27"></span>
-                    <span class="path28"></span>
-                    <span class="path29"></span>
-                    <span class="path30"></span>
-                    <span class="path31"></span>
-                    <span class="path32"></span>
-                    <span class="path33"></span>
-                    <span class="path34"></span>
-                    <span class="path35"></span>
-                    <span class="path36"></span>
-                    <span class="path37"></span>
-                    <span class="path38"></span>
-                    <span class="path39"></span>
-                    <span class="path40"></span>
-                    <span class="path41"></span>
-                    <span class="path42"></span>
-                    <span class="path43"></span>
-                    <span class="path44"></span>
-                    <span class="path45"></span>
-                    <span class="path46"></span>
-                    <span class="path47"></span>
-                    <span class="path48"></span>
-                    <span class="path49"></span>
-                    <span class="path50"></span>
+                    <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span><span class="path20"></span><span class="path21"></span><span class="path22"></span><span class="path23"></span><span class="path24"></span><span class="path25"></span><span class="path26"></span><span class="path27"></span><span class="path28"></span><span class="path29"></span><span class="path30"></span><span class="path31"></span><span class="path32"></span><span class="path33"></span><span class="path34"></span><span class="path35"></span><span class="path36"></span><span class="path37"></span><span class="path38"></span><span class="path39"></span><span class="path40"></span><span class="path41"></span><span class="path42"></span><span class="path43"></span><span class="path44"></span><span class="path45"></span><span class="path46"></span><span class="path47"></span><span class="path48"></span><span class="path49"></span><span class="path50"></span>
                 </span>
                 <span class="blind">미국</span>
             </button>
--- Components/Theme/views/_sidebar.blade.php
+++ Components/Theme/views/_sidebar.blade.php
@@ -4,43 +4,51 @@
 
         <nav>
             <ul class="list-gnb reset-list">
-                <li>
-                    <a href="#" class="link-gnb">HOME</a>
-                    <ul class="list-lnb reset-list">
-                        <li>
-                            <a href="#" class="link-lnb">HOME</a>
-                            <ul class="list-lnb-depth reset-list">
-                                <li><a href="#" class="link-lnb-depth">HOME</a></li>
-                                <li><a href="#" class="link-lnb-depth">HOME</a></li>
-                                <li><a href="#" class="link-lnb-depth">HOME</a></li>
+                {{-- 1st menu --}}
+                @foreach(menu_list($config->get('mainMenu')) as $menu1st)
+                    <li>
+                        <a href="{{ url($menu1st['url']) }}" @if($menu1st['target'] !== '_self') target="{{ $menu1st['target'] }}" @endif class="link-gnb">{{ $menu1st['link'] }}</a>
+
+                        {{-- 2nd menu --}}
+                        @if(count($menu1st['children']))
+                            <ul class="list-lnb reset-list">
+                                @foreach($menu1st['children'] as $menu2nd)
+                                    <li>
+                                        <a  href="{{ url($menu2nd['url']) }}" @if($menu2nd['target'] !== '_self') target="{{ $menu2nd['target'] }}" @endif class="link-lnb">{{ $menu2nd['link'] }}</a>
+
+                                        {{-- 3rd menu --}}
+                                        @if(count($menu2nd['children']))
+                                            <ul class="list-lnb-depth reset-list">
+                                                @foreach($menu2nd['children'] as $menu3rd)
+                                                    <li><a href="{{ url($menu3rd['url']) }}" @if($menu3rd['target'] !== '_self') target="{{ $menu3rd['target'] }}" @endif class="link-lnb-depth">{{ $menu3rd['link'] }}</a></li>
+                                                @endforeach
+                                            </ul>
+                                        @endif
+                                    </li>
+                                @endforeach
                             </ul>
-                        </li>
-                        <li><a href="#" class="link-lnb">HOME</a></li>
-                        <li><a href="#" class="link-lnb">HOME</a></li>
-                        <li><a href="#" class="link-lnb">HOME</a></li>
-                    </ul>
-                </li>
-                <li><a href="#" class="link-gnb">Profile</a></li>
-                <li><a href="#" class="link-gnb">Discography</a></li>
-                <li><a href="#" class="link-gnb">Videos</a></li>
-                <li><a href="#" class="link-gnb">Gallery</a></li>
+                        @endif
+                    </li>
+                @endforeach
             </ul>
         </nav>
 
-        <ul class="list-tool reset-list">
-            <li><a href="#" class="link-tool">회원가입</a> / <a href="#" class="link-tool">로그인</a></li>
-            <li><a href="#" class="link-tool">서비스 약관 / 이용정책</a></li>
-            <li><a href="#" class="link-tool">고객센터</a></li>
-            <li><a href="#" class="link-tool">운영규칙</a></li>
-        </ul>
+        {{-- SNB --}}
+        @if($config->get('useSnb', 'N') === 'Y')
+            <ul class="list-tool reset-list">
+                @foreach(menu_list($config->get('sidebarMenu')) as $menu)
+                    <li><a href="{{ url($menu['url']) }}" @if($menu['target'] !== '_self') target="{{ $menu['target'] }}" @endif class="link-tool">{{ $menu['link'] }}</a></li>
+                @endforeach
+            </ul>
+        @endif
+        {{-- // SNB --}}
 
         <ul class="list-sns reset-list">
-            <li class="item-sns"><a href="#" class="link-sns"><span class="blind">twitter</span><span class="icon_sns_twitter"></span></a></li>
-            <li class="item-sns"><a href="#" class="link-sns"><span class="blind">youtube</span><span class="icon_sns_youtube"></span></a></li>
-            <li class="item-sns"><a href="#" class="link-sns"><span class="blind">instagram</span><span class="icon_sns_instagram"></span></a></li>
-            <li class="item-sns"><a href="#" class="link-sns"><span class="blind">facebook</span><span class="icon_sns_facebook"></span></a></li>
+            @if($config->get('socialTwitter')) <li class="item-sns"><a href="{{ $config->get('socialTwitter') }}" target="_blank" class="link-sns"><span class="blind">twitter</span><span class="icon_sns_twitter"></span></a></li> @endif
+            @if($config->get('socialYoutube')) <li class="item-sns"><a href="{{ $config->get('socialYoutube') }}" target="_blank" class="link-sns"><span class="blind">youtube</span><span class="icon_sns_youtube"></span></a></li> @endif
+            @if($config->get('socialInstagram')) <li class="item-sns"><a href="{{ $config->get('socialInstagram') }}" target="_blank" class="link-sns"><span class="blind">instagram</span><span class="icon_sns_instagram"></span></a></li> @endif
+            @if($config->get('socialFacebook')) <li class="item-sns"><a href="{{ $config->get('socialFacebook') }}" target="_blank" class="link-sns"><span class="blind">facebook</span><span class="icon_sns_facebook"></span></a></li> @endif
         </ul>
-
     </div>
     <span class="bg-sidebar"></span>
 </aside>
