72757d28324e05431be24ccd00d94022cb07a858
bnu <bnufactory@gmail.com>
2018-10-29 18:17:04 +0900

    로고 마크업 적용

--- Components/Theme/views/_header.blade.php
+++ Components/Theme/views/_header.blade.php
@@ -1,17 +1,8 @@
 <header class="header-tfc">
     <button type="button" class="btn-menu" title="메뉴"><span class="icon_menu"></span><span class="blind">메뉴</span></button>
     <h1 class="logo">
-        <a href="{{ url('/') }}" class="link-logo">
-            @if($config->get('logoType', 'text') === 'text')
-                {{ xe_trans($config->get('logoText', 'Together')) }}
-            @else
-                @if($config->get('logoImage.path'))
-                    <img src="{{ $config->get('logoImage.path') }}" alt="{{ xe_trans($config->get('logoText', '')) }}" />
-                @else
-                    {{ xe_trans($config->get('logoText', 'Together')) }}
-                @endif
-            @endif
-        </a>
+        <a href="{{ url('/') }} " class="link-logo" @if($config->get('logoImage.path')) style="background-image:url('{{ $config->get('logoImage.path') }}')" @endif>{{ xe_trans($config->get('logoText', '')) }}</a>
+
         @if($config->get('slogan'))
             <span class="greet">{{ xe_trans($config->get('slogan')) }}</span>
         @endif
