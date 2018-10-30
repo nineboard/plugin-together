50685a3d87738bab0c0078d085595af36356142d
bnu <bnufactory@gmail.com>
2018-10-30 19:23:25 +0900

    변경된 게시판 리스트 적용 및 view 마크업 적용

--- Components/Skins/Board/Together/views/_paging.blade.php
+++ Components/Skins/Board/Together/views/_paging.blade.php
@@ -2,7 +2,7 @@
 <!-- board-paging-tfc -->
 <div class="board-paging-tfc">
     @if($paginator->currentPage() <= 1)
-        <span class="link-arr link-prev"><i class="xi-angle-left"><span class="blind">이전</span></i></span>
+        <a class="link-arr link-prev link-nomore"><i class="xi-angle-left"><span class="blind">이전</span></i></a>
     @else
         <a href="{{ $paginator->previousPageUrl() }}" class="link-arr link-prev"><i class="xi-angle-left"><span class="blind">이전</span></i></a>
     @endif
@@ -22,7 +22,11 @@
             @endif
         @endforeach
     </div>
-    <a href="#" class="link-arr link-next"><i class="xi-angle-right"><span class="blind">다음</span></i></a>
+    @if(!$paginator->hasMorePages())
+        <a class="link-arr link-next link-nomore"><i class="xi-angle-right"><span class="blind">다음</span></i></a>
+    @else
+        <a href="#" class="link-arr link-next"><i class="xi-angle-right"><span class="blind">다음</span></i></a>
+    @endif
 </div>
 <!-- // board-paging-tfc -->
 @endif
