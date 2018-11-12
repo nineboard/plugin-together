a9e8b00140955671ad9f31dd4bcbd69250136419
bnu <bnufactory@gmail.com>
2018-11-12 15:16:40 +0900

    #1 헤더 회원 메뉴 추가

--- Components/Theme/views/_header.blade.php
+++ Components/Theme/views/_header.blade.php
@@ -13,5 +13,20 @@
             <span class="greet">{{ xe_trans($config->get('slogan')) }}</span>
         @endif
     </h1>
-    <button type="button" class="btn-login" title="로그인"><span class="icon_login"></span><span class="text_login">로그인</span></button>
+
+    <div class="user-menu-dropdown">
+        <button type="button" class="btn-login" title="{{ xe_trans('xe::user') }}"><span class="icon_login"></span></button>
+        <ul class="user-menu">
+            @if(Auth::check())
+                @if(auth()->user()->isAdmin())
+                    <li><a href="{{ route('settings') }}" class="plugin"><i class="xi-cog"></i> {{ xe_trans('xe::settings') }}</a></li>
+                @endif
+                <li><a href="{{ route('user.profile', ['user' => auth()->id()]) }}">{{ xe_trans('xe::myProfile') }}</a></li>
+                <li><a href="{{ route('user.settings') }}">{{ xe_trans('xe::mySettings') }}</a></li>
+                <li><a href="{{ route('logout') }}">{{ xe_trans('xe::logout') }}</a></li>
+            @else
+                <li><a href="{{ route('login') }}">{{ xe_trans('xe::login') }}</a></li>
+            @endif
+        </ul>
+    </div>
 </header>
