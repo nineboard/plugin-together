64eb9ae394d20890872304fac22eb645f95a3493
sirwoongke <fzwoongbin@naver.com>
2019-03-18 15:36:53 +0900

    together 게시판 스킨 삭제

--- Components/Skins/Board/Together/views/_frame.blade.php
+++
@@ -1,9 +0,0 @@
-{{ XeFrontend::css('plugins/together/assets/css/board.css')->load() }}
-
-<!-- section-board-tfc -->
-<section class="section-board-tfc">
-    <!-- board-notice-tfc -->
-        @section('content')
-            {!! isset($content) ? $content : '' !!}
-        @show
-</section>
--- Components/Skins/Board/Together/views/votedUsers.blade.php
+++
@@ -1,6 +0,0 @@
-<a href="#" data-toggle="xe-page-modal" data-url="{{ $urlHandler->get('votedModal', ['option' => $option, 'id' => $item->id]) }}" data-params="{}" data-callback="AssentVirtualGrid.init" class="link-profile">
-    @foreach ($logs as $log)
-        <span class="thumbnail" style="background-image:url('{{ $log->user->getProfileImage() }}')"></span>
-    @endforeach
-    좋아요 {{ number_format($item->assent_count) }}명
-</a>
