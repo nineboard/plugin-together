6e98d5fbb1f2a9a529d6a884376c556a26bb1f0b
xharpenParksuhyeon <parksu@xharpen.com>
2021-07-24 04:46:55 +0000

    #12 togther 테마 디데이 배너 미작동

--- Components/Skins/Banner/Dday/views/setting.blade.php
+++ Components/Skins/Banner/Dday/views/setting.blade.php
@@ -1,2 +1,8 @@
 dday
-{{ uio('formText', ['name'=>'dday_at', 'type'=>'date', 'value'=>($item['dday_at'] ? $item['dday_at'] : '2038-01-19')]) }}
+{{
+    uio('formText', [
+        'name'=>'dday_at',
+        'type'=>'date',
+        'value'=>(is_array($item) && array_key_exists('dday_at', $item) ? $item['dday_at'] : '2038-01-19'
+    )])
+}}
